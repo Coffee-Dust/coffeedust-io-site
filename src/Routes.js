ade20b1f1ad6415db5212f00000dc818d8a5c274
github username <d24dust@hotmail.com>
2021-06-20 17:07:07 -0400

    add: /about route

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import AboutPage from './pages/about';
 
 function Routes() {
   return (
     <Switch>
-      
+      <Route path="/about">
+        <AboutPage />
+      </Route>
     </Switch>
   );
 }
