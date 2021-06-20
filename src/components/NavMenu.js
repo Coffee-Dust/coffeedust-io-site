f8b310fc617fcb63aad6ebe22826a6b56c455b9b
github username <d24dust@hotmail.com>
2021-06-20 16:07:04 -0400

    add: <Link> to /about

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 function NavMenu() {
   return (
     <div className="App-nav-menu">
-      
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">about me</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
