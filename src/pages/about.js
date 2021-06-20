f20ec81e0687ee6163eafeb9e00a498ac1b4c290
github username <d24dust@hotmail.com>
2021-06-20 17:29:55 -0400

    create: about page with filler text

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h2>About Me(Some hipster ipsum)</h2>
+      <p>I'm baby brunch activated charcoal offal tousled cliche. Gochujang banh mi godard master cleanse gentrify quinoa aesthetic enamel pin. Celiac mlkshk trust fund fingerstache. Next level kinfolk knausgaard, XOXO raclette gluten-free affogato lumbersexual ramps viral raw denim coloring book. Venmo viral everyday carry readymade kogi tattooed. Mlkshk palo santo affogato selvage bicycle rights unicorn bushwick hell of listicle godard. Viral deep v umami, listicle cold-pressed chartreuse chia aesthetic irony polaroid 90's mumblecore sartorial.</p>
+      <p>Jean shorts venmo vice messenger bag heirloom hexagon single-origin coffee chambray coloring book raw denim bespoke chartreuse neutra yuccie. Cold-pressed hot chicken meditation palo santo, succulents plaid cronut ethical chia lomo pop-up pok pok. Food truck trust fund selfies actually woke jean shorts hella, iceland unicorn. Live-edge austin tumblr intelligentsia authentic.</p>
+      <p>Dummy text? More like dummy thicc text, amirite?</p>
+    </div>
+  );
+}
+
+export default AboutPage;
