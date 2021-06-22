5d30f761b394664079f267d6f0b4306e56cb4944
github username <d24dust@hotmail.com>
2021-06-22 15:25:27 -0400

    add: all future navlinks

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 
 function NavMenu() {
   return (
     <div className="App-nav-menu">
       <nav>
-        <Link to="/about">about me</Link>
-        
+        <NavLink to="/about">about me</NavLink>
+        <NavLink to="/blog">my blog</NavLink>
+        <NavLink to="/projects">my projects</NavLink>
+        <NavLink to="/resume">my resume</NavLink>
       </nav>
     </div>
   );
