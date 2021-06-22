637f261f05981a51245718f2d10ca7daed5acebd
github username <d24dust@hotmail.com>
2021-06-22 18:34:15 -0400

    add: renderThumbnail method

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -3,5 +3,19 @@ class BlogPost {
     this.data = data
   }
 
+  get renderThumbnail() {
+    const removeHTMLTags = desc => (
+      desc.split("")
+        .filter((v, i, a) => !(v === "<" || v === ">" || a[i - 1] === "<" || a[i + 1] === ">"))
+        .join("")
+    )
+    return (
+      <div id={this.data.guid} className="Blogs-thumbnail">
+        <img src={this.data.thumbnail} alt="" />
+        <h4>{this.data.title}</h4>
+        <p>{removeHTMLTags(this.data.description.slice(0, 49))}...</p>
+      </div>
+    )
+  }
 }
 export default BlogPost;
