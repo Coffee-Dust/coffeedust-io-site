f881b051d805a09a12bdac6e806b73685c71cdee
github username <d24dust@hotmail.com>
2021-06-22 19:14:17 -0400

    add: thumbnailOnClickEvent setsState to expandView

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -7,7 +7,8 @@ class BlogPost extends Component {
   }
 
   thumbnail = _=> (
-    <div key={this.data.guid} className="Blogs-thumbnail">
+    <div key={this.data.guid} className="Blogs-thumbnail" 
+    onClick={_=> this.setState({expandedViewIsActive: true})} >
       <img src={this.data.thumbnail} alt="" />
       <h4>{this.data.title}</h4>
       <p>{removeHTMLTags(this.data.description.slice(0, 49))}...</p>
