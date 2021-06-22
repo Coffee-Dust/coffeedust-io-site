488d3b1662f9acd5e1321ea4ed34bd0014994ce7
github username <d24dust@hotmail.com>
2021-06-22 15:31:24 -0400

    import: BlogPage

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AboutPage from './pages/about';
+import BlogPage from './pages/blog';
 
 function Routes() {
   return (
