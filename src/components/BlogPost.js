c178b6a3f35bcc26e917645d839b3032b208f778
github username <d24dust@hotmail.com>
2021-06-22 19:09:15 -0400

    add: thumbnail method for render

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 class BlogPost extends Component {
 
+  thumbnail = _=> (
+    <div key={this.data.guid} className="Blogs-thumbnail">
+      <img src={this.data.thumbnail} alt="" />
+      <h4>{this.data.title}</h4>
+      <p>{removeHTMLTags(this.data.description.slice(0, 49))}...</p>
+    </div>
+  )
+
   render() {
     return (
       <div>
