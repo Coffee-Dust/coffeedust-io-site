3110301a47df7967addc7ac7cc4f02e1c90fa620
github username <d24dust@hotmail.com>
2021-06-24 13:04:14 -0400

    change: html page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,35 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div>
-      <h2>About Me(Some hipster ipsum)</h2>
-      <p>I'm baby brunch activated charcoal offal tousled cliche. Gochujang banh mi godard master cleanse gentrify quinoa aesthetic enamel pin. Celiac mlkshk trust fund fingerstache. Next level kinfolk knausgaard, XOXO raclette gluten-free affogato lumbersexual ramps viral raw denim coloring book. Venmo viral everyday carry readymade kogi tattooed. Mlkshk palo santo affogato selvage bicycle rights unicorn bushwick hell of listicle godard. Viral deep v umami, listicle cold-pressed chartreuse chia aesthetic irony polaroid 90's mumblecore sartorial.</p>
-      <p>Jean shorts venmo vice messenger bag heirloom hexagon single-origin coffee chambray coloring book raw denim bespoke chartreuse neutra yuccie. Cold-pressed hot chicken meditation palo santo, succulents plaid cronut ethical chia lomo pop-up pok pok. Food truck trust fund selfies actually woke jean shorts hella, iceland unicorn. Live-edge austin tumblr intelligentsia authentic.</p>
-      <p>Dummy text? More like dummy thicc text, amirite?</p>
-    </div>
+    <article>
+      <h2>Nice to meet you! 👋</h2>
+      <p>My name is John Dust. I am a Software Engineer who's passion to code might even be greater than my passion for coffee.</p>
+
+      <div>
+        <h4>Languages I am well versed in</h4>
+        <ul>
+          <li>Javascript</li>
+          <li>Ruby</li>
+          <li>HTML/CSS</li>
+          <li>Swift</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Experienced in these technologies/frameworks</h4>
+        <ul>
+          <li>Object Oriented Programming</li>
+          <li>Ruby on Rails</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>(even more...)</li>
+        </ul>
+      </div>
+
+      {/* <p>I'm baby brunch activated charcoal offal tousled cliche. Gochujang banh mi godard master cleanse gentrify quinoa aesthetic enamel pin. Celiac mlkshk trust fund fingerstache. Next level kinfolk knausgaard, XOXO raclette gluten-free affogato lumbersexual ramps viral raw denim coloring book. Venmo viral everyday carry readymade kogi tattooed. Mlkshk palo santo affogato selvage bicycle rights unicorn bushwick hell of listicle godard. Viral deep v umami, listicle cold-pressed chartreuse chia aesthetic irony polaroid 90's mumblecore sartorial.</p> */}
+      {/* <p>Jean shorts venmo vice messenger bag heirloom hexagon single-origin coffee chambray coloring book raw denim bespoke chartreuse neutra yuccie. Cold-pressed hot chicken meditation palo santo, succulents plaid cronut ethical chia lomo pop-up pok pok. Food truck trust fund selfies actually woke jean shorts hella, iceland unicorn. Live-edge austin tumblr intelligentsia authentic.</p> */}
+      {/* <p>Dummy text? More like dummy thicc text, amirite?</p> */}
+    </article>
   );
 }
 
