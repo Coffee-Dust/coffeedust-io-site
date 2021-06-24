91fa310d6803ac3fbdac81dd15ca56d622a640e1
github username <d24dust@hotmail.com>
2021-06-24 12:59:59 -0400

    feat: fetches blog posts and stores in state

--- src/containers/Blogs.js
+++ src/containers/Blogs.js
@@ -3,6 +3,17 @@ import BlogPost from '../models/blogPost';
 
 class BlogsContainer extends Component {
 
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@coffeedust").then(r=>r.json()).then(data=> {
+      const posts = data.items.map((blogData, idx)=> <BlogPost {...blogData} key={idx} />)
+      this.setState(s=> ({...s, posts: posts}))
+    })
+  }
+
   render() {
     return (
       <div>
