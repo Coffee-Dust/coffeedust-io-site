4001f9464fba57c5bf54f07cae6e02814e8c462a
github username <d24dust@hotmail.com>
2021-07-01 13:29:35 -0400

    add: className to div render

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ProjectsContainer extends Component {
   render() {
     return (
-      <div>
+      <div className="Projects-container">
         
       </div>
     );
