bc30e1c82d381369e6eab039e2223aba2caf5f78
github username <d24dust@hotmail.com>
2021-07-01 13:36:37 -0400

    add: /projects route

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AboutPage from './pages/about';
 import BlogPage from './pages/blog';
+import ProjectsPage from './pages/projects'
 
 function Routes() {
   return (
@@ -11,6 +12,9 @@ function Routes() {
       </Route>
       <Route path="/blog">
         <BlogPage />
+      </Route>
+      <Route path="/projects">
+        <ProjectsPage />
       </Route>
     </Switch>
   );
