4987464fa07831e265e43c497d0826e1ac56d801
github username <d24dust@hotmail.com>
2021-07-04 18:38:25 -0500

    add: className & render: ProjectsContainer

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProjectsContainer from '../containers/Projects';
 
 function ProjectsPage() {
   return (
-    <div>
-      
+    <div className="Page-content">
+      <ProjectsContainer />
     </div>
   );
 }
