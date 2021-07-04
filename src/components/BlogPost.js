0f9d0a70233111e6848c106204a97af76cca3ee9
github username <d24dust@hotmail.com>
2021-07-03 20:20:13 -0500

    refactor: seperated BlogThumbnail functionality & expanded BlogPost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,38 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BlogPost extends Component {
+function BlogPost({title, content, thumbnail}) {
+  return (
+    <div className="Blogs-post blur-backdrop">
+      <img src={thumbnail} alt="" />
+      <h2>{title}</h2>
+      <article dangerouslySetInnerHTML={{ __html: content }} />
 
-  state = {
-    expandedViewIsActive: false
-  }
-
-  toggleExpandedView = event=> {
-    if (event.currentTarget.classList.contains("Blogs-thumbnail")) {
-      event.currentTarget.classList.toggle("expanded")
-      event.currentTarget.parentNode.classList.toggle("column-view")
-    }
-  }
-
-  removeHTMLTags = desc=> (
-    desc.split("")
-    .filter((v, i, a) => !(v === "<" || v === ">" || a[i - 1] === "<" || a[i + 1] === ">"))
-    .join("")
-  )
-
-
-  render() {
-    return (
-    <div className="Blogs-thumbnail sun-box-shadow" 
-    style={{ backgroundImage: `url(${this.props.thumbnail})` }}
-    onClick={this.toggleExpandedView} >
-      <h2>{this.props.title}</h2>
-      <p>{this.removeHTMLTags(this.props.description.slice(0, 49))}...</p>
-
-      <article dangerouslySetInnerHTML={{__html: this.props.content}} />
     </div>
-    );
-  }
-
+  );
 }
 
 export default BlogPost;
--- src/components/BlogThumbnail.js
+++ src/components/BlogThumbnail.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function BlogThumbnail() {
+function BlogThumbnail({idx, title, description, thumbnail, openPost}) {
+  const descCharLimit = 49
+  // Removes HTML tags from description
+  description = description.slice(0, descCharLimit).split("")
+  .filter((v, i, a) => !(v === "<" || v === ">" || a[i - 1] === "<" || a[i + 1] === ">"))
+  .join("") + "..."
+
   return (
-    <div>
-      
+    <div 
+    id={idx}
+    className="Blogs-thumbnail"
+    style={{ backgroundImage: `url(${thumbnail})` }}
+    onClick={openPost}
+    >
+      <h2>{title}</h2>
+      <p>{description}</p>
     </div>
   );
 }
