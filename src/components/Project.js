8c402e7ca64085130a18b65b32d7a2f6e7658f6f
github username <d24dust@hotmail.com>
2021-07-04 18:32:52 -0500

    refactor: moved .Project-info into seperate components

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import gitHubLogo from '../assets/GitHub-Logo.png'
 
 function Project(props) {
 
-  const startDemo = event=> {
-
-  }
-
-  const goToRepo = event=> {
-
-  }
-
   return (
-    <div className="Project-app-div" style={{"--animation-order": `${props.animationPos}`}}>
+    <div id={props.idx} className="Project-app-div"
+    style={{"--animation-order": `${props.animationPos}`}}
+    onClick={props.open}
+    >
       <div className="icon-shadow"></div>
       <img src={require(`./../assets/project_icons/${props.id}_icon.png`).default} alt="" />
-
-      <div className={`Project-info`} hidden={props.opened}>
-        <h2>{props.name}</h2>
-        <div className="links">
-          <button className="repo" onClick={goToRepo}> <img src={gitHubLogo} /> repo</button>
-          <button className="demo" onClick={startDemo}>Live Demo</button>
-        </div>
-        <p>{props.desc}</p>
-      </div>
     </div>
   );
 }
--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import gitHubLogo from '../assets/GitHub-Logo.png';
 
-function ProjectDetails() {
+function ProjectDetails(props) {
+
+  const startDemo = event=> {
+
+  }
+
+  const goToRepo = event=> {
+
+  }
+
   return (
-    <div>
-      
+    <div className="Project-details">
+      <img src={require(`./../assets/project_icons/${props.id}_icon.png`).default} alt="" />
+      <h2>{props.name}</h2>
+      <div className="links">
+        <button className="repo" onClick={goToRepo}> <img src={gitHubLogo} /> repo</button>
+        <button className="demo" onClick={startDemo}>Live Demo</button>
+      </div>
+      <p>{props.desc}</p>
     </div>
   );
 }
