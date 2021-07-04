9e0724d80502670474129594b08240b16c5918a4
github username <d24dust@hotmail.com>
2021-07-03 20:40:29 -0500

    add: onclick closePost callback

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-function BlogPost({title, content, thumbnail}) {
+function BlogPost({title, content, thumbnail, closePost}) {
   return (
     <div className="Blogs-post blur-backdrop">
+      <button className="back" onClick={closePost}>&lt;- back</button>
       <img src={thumbnail} alt="" />
       <h2>{title}</h2>
       <article dangerouslySetInnerHTML={{ __html: content }} />
