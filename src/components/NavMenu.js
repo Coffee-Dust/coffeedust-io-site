4b16b55402cecf3416d107978f9794a507da173d
github username <d24dust@hotmail.com>
2021-07-05 13:16:25 -0500

    import: SocialLinks

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavLink} from "react-router-dom"
 import logo from '../assets/logo.svg';
+import SocialLinks from './SocialLinks';
 
 function NavMenu() {
   return (
