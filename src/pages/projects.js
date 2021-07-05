e867e061445f6c8dd33ef5ac6ea85dc07af1b952
github username <d24dust@hotmail.com>
2021-07-05 13:57:44 -0500

    add: page h1 title

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import ProjectsContainer from '../containers/Projects';
 function ProjectsPage() {
   return (
     <div className="Page-content">
+      <h1>my projects</h1>
       <ProjectsContainer />
     </div>
   );
