e0e4b2b2d55f25b5bd6fcea7ee418ce1a17ed3b8
github username <d24dust@hotmail.com>
2021-07-05 13:17:29 -0500

    feat: sets CSS-var --scale-factor on resizing of window

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,10 +4,21 @@ import logo from '../assets/logo.svg';
 import SocialLinks from './SocialLinks';
 
 function NavMenu() {
+
+  const navRef = React.useRef()
+  const calcScaleFactor = element=> {
+    const scaleFactor = getComputedStyle(element.current).width.slice(0, 3) * 0.0065
+    return `--scale-factor: ${(scaleFactor > 1.1) ? scaleFactor : 1.1};`
+  }
+  React.useEffect(_=> {
+    navRef.current.style = calcScaleFactor(navRef)
+    window.addEventListener('resize', _=> navRef.current.style = calcScaleFactor(navRef))
+  },[])
+
   return (
     <div className="App-nav-menu">
       <img src={logo} className="App-logo" alt="logo" />
-      <nav>
+      <nav ref={navRef}>
         <NavLink to="/about">about me</NavLink>
         <NavLink to="/blog">blog</NavLink>
         <NavLink to="/projects">projects</NavLink>
