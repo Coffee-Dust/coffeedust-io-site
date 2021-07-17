861d6a0c93b68dff8ca8d6cb320d0fd834e155e1
github username <d24dust@hotmail.com>
2021-07-17 14:31:24 -0400

    feat: toggle NavMenu functional

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,14 +3,24 @@ import '../assets/css/effects.css';
 import NavMenu from '../components/NavMenu';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../Routes';
+import logo from '../assets/logo.svg';
 
 function App() {
+
+  const toggleMenu = event=> {
+    if (event.currentTarget.parentNode.classList.contains("App-nav-menu")) {
+      event.currentTarget.parentNode.classList.toggle("opened")
+    } else {
+      document.querySelector(".App-nav-menu").classList.remove("opened")
+    }
+  }
+  
   return (
     <Router>
       <div className="App">
         <img src={logo} className="App-logo-static" alt="logo" />
-        <NavMenu />
-        <main className="App-content-container blur-backdrop">
+        <NavMenu toggleMenu={toggleMenu} />
+        <main className="App-content-container blur-backdrop" onClick={toggleMenu}>
           <Routes />
         </main>
         <h3 className="version-msg">public beta 2.0</h3>
