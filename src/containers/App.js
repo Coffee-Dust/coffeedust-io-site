fc0c6b64d606464a40b1763cbb133484ecfef6d0
github username <d24dust@hotmail.com>
2021-07-17 14:10:48 -0400

    add: static logo for mobile

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <img src={logo} className="App-logo-static" alt="logo" />
         <NavMenu />
         <main className="App-content-container blur-backdrop">
           <Routes />
