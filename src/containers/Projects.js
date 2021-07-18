d56051358f62f68bbbed24f15a530f922daf9014
github username <d24dust@hotmail.com>
2021-07-18 12:46:21 -0400

    feat: handles REST routes for /projects

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -10,11 +10,34 @@ class ProjectsContainer extends Component {
     opened: false
   }
 
+  componentDidMount() {
+    if (this.props.match) {
+      this.displayProjectFromSlug()
+    }
+  }
+
+  displayProjectFromSlug = _=> {
+    console.log(this.props)
+    const projectInfoIndex = projectInfo.projects.findIndex(p=> p.id === this.props.match.params.slug)
+    if (this.props.match.params.slug && projectInfoIndex) {
+      this.setState(s => ({ ...s, opened: projectInfoIndex}))
+    }
+  }
+
   toggleProjectDetails = (event)=> {
     if (this.state.opened) {
       this.setState(s => ({ ...s, opened: false }))
+
+      // This fixes checks to see if the project was visted from direct URL or locally
+      if (window.history.state && window.history.state.openedFromLocalPage) {
+        window.history.back()
+      } else {
+        // It was visited from direct url, so no history.
+        window.location = "/projects"
+      }
     } else {
       this.setState(s => ({ ...s, opened: event.target.parentNode.id}))
+      window.history.pushState({openedFromLocalPage: true}, null, `projects/${projectInfo.projects[event.target.parentNode.id].id}`)
     }
     event.target.parentNode.classList.toggle("opened")
   }
