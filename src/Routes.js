f73051420fb38dd55aa852dfbf35f358c0e6ec16
github username <d24dust@hotmail.com>
2021-07-18 12:47:21 -0400

    import: redirect from react-router-dom

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AboutPage from './pages/about';
 import BlogPage from './pages/blog';
 import ProjectsPage from './pages/projects'
