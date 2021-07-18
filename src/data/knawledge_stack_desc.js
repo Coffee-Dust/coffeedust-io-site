d123eaaddbb3e5472a454b067608c896761d6f11
github username <d24dust@hotmail.com>
2021-07-18 19:17:46 -0400

    add: ruby desc

--- src/data/knawledge_stack_desc.js
+++ src/data/knawledge_stack_desc.js
@@ -2,7 +2,21 @@ import React from 'react'
 const stackKnawledgeDesc = {
   "ruby": (
   <article>
-    <h4>You be nice cuz matz is nice</h4>
+    <h2>Ruby</h2>
+    <p>
+      I learned this language during my curriculum at Flatiron School and it is probably the language I have used the most.<br/>
+      In fact, this would be the second programming language I learned, after Swift.(not counting html or css)<br/>
+      I do enjoy the simple syntax of the language, but I do actually prefer strongly-typed languages like Swift.<br/>
+      <br/>
+      I've learned most of the ends and outs of this language, such as its method based approach to properties, and some more complicated things, like it's concurrent multi-threading system.<br/>
+      <br/>
+      Almost every major project I've built uses Ruby in some way. However, the projects I have below were mostly made with just Ruby, no major Frameworks.
+    </p>
+    <div className="Page-about-desc-project-icons">
+      <h4>Projects that used this language.</h4>
+      <a href="/projects/inventory_manager"><img src={require(`./../assets/project_icons/inventory_manager_icon.png`).default} alt="" /></a>
+      <a href="/projects/local_coffeeshop"><img src={require(`./../assets/project_icons/local_coffeeshop_icon.png`).default} alt="" /></a>
+    </div>
   </article>
   ),
   "js": (
