f59733e73f72d0eb218dd167579accf522e6645a
github username <d24dust@hotmail.com>
2021-07-18 13:13:22 -0400

    change: file from json to .js

---
+++ src/data/knawledge_stack_desc.js
@@ -0,0 +1,35 @@
+import React from 'react'
+const stackKnawledgeDesc = {
+  "ruby": (
+  <article>
+    <h4>You be nice cuz matz is nice</h4>
+  </article>
+  ),
+  "js": (
+  <article>
+    <h4>Dumpster fire</h4>
+  </article>
+  ),
+  "swift": (
+  <article>
+
+  </article>
+  ),
+  "rails": (
+  <article>
+
+  </article>
+  ),
+  "react": (
+  <article>
+
+  </article>
+  ),
+  "html": (
+  <article>
+
+  </article>
+  )
+}
+
+export default stackKnawledgeDesc
