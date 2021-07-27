a92de9320e13b1d248ef8cfb041ac4bf5945f0a6
github username <d24dust@hotmail.com>
2021-07-27 16:06:09 -0400

    create: ResumePage component content page

---
+++ src/pages/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ResumePage(props) {
+  return (
+    <div className="Page-content">
+      <h1>my resumé</h1>
+    </div>
+  );
+}
+
+export default ResumePage;
