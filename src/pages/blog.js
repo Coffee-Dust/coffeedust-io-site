31061adb44bdf77df758407acb8e8b74f4a43e35
github username <d24dust@hotmail.com>
2021-07-30 13:41:11 -0400

    add: button link to medium.com blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,7 @@ function BlogPage() {
   return (
     <div className="Page-content">
       <h1>my blogs</h1>
+      <a id="mediumLinkButton" href="https://coffeedust.medium.com" target="_blank" rel="noreferrer"><button className="styled-button-light">Blogs hosted via <img src={require("../assets/mediumLogo.png").default} alt="See blogs on Medium.com" /></button></a>
       <BlogsContainer />
     </div>
   );
