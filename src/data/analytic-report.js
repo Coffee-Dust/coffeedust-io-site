7e63c546bbea9ca6fea5bc7ccceef8239de65e14
github username <d24dust@hotmail.com>
2021-08-31 18:06:54 -0400

    add: setter & getter methods for eventType

--- src/data/analytic-report.js
+++ src/data/analytic-report.js
@@ -9,6 +9,18 @@ class AnalyticReport {
     this.eventDetails = data.details
   }
 
+  get eventType() {
+    return this._eventType
+  }
+
+  set eventType(event) {
+    if (this.validEvents.includes(event)) {
+      this._eventType = event
+    } else {
+      throw new Error("<InvalidAnalyticsReportEventType> unknown or invalid eventType was assigned to a report")
+    }
+  }
+
 }
 
 export default AnalyticReport
