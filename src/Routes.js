a5ccefbd04ffe3d81300c7f3c097ecf16c5396d0
github username <d24dust@hotmail.com>
2021-09-02 13:47:42 -0400

    feat: onWindowLocationChange, reportsEvent via AnalyticsReporter

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,20 @@ import ProjectsPage from './pages/projects'
 import ResumePage from './pages/resume';
 
 function Routes() {
+
+  // <Analytics> onPageLoad event callback
+  const windowLocation = useLocation();
+  React.useEffect(_=> {
+    // Checks for direct project visit, i.e. cd.io/projects/:slug
+    if (window.location.pathname !== "/projects" && window.location.pathname.includes("/projects")) {
+      const projectNameFromSlug = window.location.pathname.slice(10)
+      AnalyticsReporter.reportEvent("pageLoad", {pageName: "/projects", directProjectLink: true, projectName: projectNameFromSlug}).then(e => console.log("dopimng athjigsk", e))
+      AnalyticsReporter.reportEvent("projectDetailsClick", {projectName: projectNameFromSlug})
+    } else {
+      AnalyticsReporter.reportEvent("pageLoad", {pageName: window.location.pathname})
+    }
+  }, [windowLocation]);
+
   return (
     <Switch>
       <Route exact path="/">
