f458a1ecc39334a3b6a3b6057383d8dad3293a68
John Dust <d24dust@hotmail.com>
2021-09-16 13:57:50 -0400

    change: innerText for mediumLinkButton

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,7 +5,7 @@ function BlogPage() {
   return (
     <div className="Page-content">
       <h1>my blogs</h1>
-      <a id="mediumLinkButton" href="https://coffeedust.medium.com" target="_blank" rel="noreferrer"><button className="styled-button-light">Blogs hosted via <img src={require("../assets/mediumLogo.png").default} alt="See blogs on Medium.com" /></button></a>
+      <a id="mediumLinkButton" href="https://coffeedust.medium.com" target="_blank" rel="noreferrer"><button className="styled-button-light">View all my blogs on <img src={require("../assets/mediumLogo.png").default} alt="See blogs on Medium.com" /></button></a>
       <BlogsContainer />
     </div>
   );
